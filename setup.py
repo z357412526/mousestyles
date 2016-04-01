7a5fc22287fcda6497065032967496afea3e49ce
Chih-Hui Wang <jjason88105@gmail.com>
2016-04-01 15:42:34 -0700

    fix travis CI

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@
 if __name__ == "__main__":
     from setuptools import setup
     setup(name="mousestyles",
-          packages=["mousestyles", "mousestyles.data", "mousestyles.data.tests"])
+          packages=["mousestyles", "mousestyles.data", "mousestyles.data.tests"],
+          package_data={'mousestyles.data': ['*.npy', '*/*/*.npy']})
 
