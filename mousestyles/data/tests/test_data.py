021d0c0ecd9fccc600a754f9098662ce91820093
Siyao Chang <changsiyao@berkeley.edu>
2016-04-09 16:59:21 -0700

    add load_intervals and load_movement functions and tests

--- mousestyles/data/tests/test_data.py
+++ mousestyles/data/tests/test_data.py
@@ -12,3 +12,13 @@ import mousestyles.data as data
 def test_all_features_loader():
     all_features = data.load_all_features()
     assert_equal(all_features.shape, (21131, 13))
+
+
+def test_intervals_loader():
+    AS = data.load_intervals('AS')
+    assert_equal(AS.shape, (1343, 5))
+
+
+def test_movement_loader():
+    movement = data.load_movement(0, 0, 0)
+    assert_equal(movement.shape, (39181, 4))
