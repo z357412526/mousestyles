1b4d7260bef5aaedd1601be991ac3f6a87823d78
Jarrod Millman <jarrod.millman@gmail.com>
2016-04-12 13:21:12 -0700

    ENH: add extension for dollar math and plotting

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -19,6 +19,7 @@ import os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
+sys.path.append(os.path.abspath('_sphinxext'))
 
 # -- General configuration ------------------------------------------------
 
@@ -35,6 +36,9 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    'sphinxcontrib.bibtex',
+    'dollarmath',
+    'plot_directive',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
