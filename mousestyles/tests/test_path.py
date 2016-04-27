8b73dea6b0865a8c5989695f436e51ff862a9bf6
Chih-Hui Wang <jjason88105@gmail.com>
2016-04-26 22:38:03 -0700

    ENH: update the test

--- mousestyles/tests/test_path.py
+++ mousestyles/tests/test_path.py
@@ -10,12 +10,18 @@ from mousestyles.path_index import path_index
 def test_path_input():
     movement = data.load_movement(0, 0, 0)
     # checking functions raise the correct errors
+    # input negative number
     with pytest.raises(ValueError) as excinfo:
         path_index(movement, -1, -1)
     assert excinfo.value.args[0] == "Input values need to be positive"
+    # input zeros
+    with pytest.raises(ValueError) as excinfo:
+        path_index(movement, 0, 0)
+    assert excinfo.value.args[0] == "Input values need to be positive"
+    # min_path_length cannot be floating number
     with pytest.raises(TypeError) as excinfo:
-        path_index(movement, 1.5, 1.5)
-    assert excinfo.value.args[0] == "Input values need to be integer"
+        path_index(movement, 1, 1.5)
+    assert excinfo.value.args[0] == "min_path_length needs to be integer"
 
 
 def test_path():
