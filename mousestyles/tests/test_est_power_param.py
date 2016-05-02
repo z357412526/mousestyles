4cf3d3b9acd5cb0d3f0a65e9fdfbbca1d1ddb043
Xinyue Zhou <xinyue233@berkeley.edu>
2016-05-02 10:31:22 -0700

    fix space again and fix module import name

--- mousestyles/tests/test_est_power_param.py
+++ mousestyles/tests/test_est_power_param.py
@@ -1,13 +1,17 @@
-from __future__ import (absolute_import, division, print_function, unicode_literals)
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
 
 import numpy as np
-from mousestyles.powerlaw import (fit_powerLaw,fit_exponential,fit)
+from mousestyles.est_power_param import (fit_powerLaw, fit_exponential, fit)
+
 
 def test_fit_powerLaw():
-	assert (fit_powerLaw(0, 0, 0) == 9.4748705008269827)
-    
+    assert (fit_powerLaw(0, 0, 0) == 9.4748705008269827)
+
+
 def test_fit_exponential():
-	assert (fit_exponential(0, 0, 0) == 7.385844980814098)
-    
+    assert (fit_exponential(0, 0, 0) == 7.385844980814098)
+
+
 def test_fit():
-	assert type(fit()) is pandas.core.frame.DataFrame
+    assert type(fit()) is pandas.core.frame.DataFrame
--- setup.py
+++ setup.py
@@ -14,62 +14,63 @@ DOWNLOAD_URL = 'https://github.com/berkeley-stat222/mousestyles'
 VERSION = '0.1dev'
 
 INSTALL_REQUIRES = [
-                    'numpy>=1.10.1',
-                    'pandas>=0.17.0',
-                    'pytest>=2.9.1',
-                    'scipy>=0.16.1',
-                    'scikit_learn>=0.17',
-                    'statsmodels>=0.6.1',
-                    'Sphinx>=1.4.1',
-                    'sphinxcontrib-bibtex>=0.3.3',
-                    'numpydoc>=0.6.0',
-                    'ghp-import>=0.4.1',
-                    ]
+    'numpy>=1.10.1',
+    'pandas>=0.17.0',
+    'pytest>=2.9.1',
+    'scipy>=0.16.1',
+    'scikit_learn>=0.17',
+    'statsmodels>=0.6.1',
+    'Sphinx>=1.4.1',
+    'sphinxcontrib-bibtex>=0.3.3',
+    'numpydoc>=0.6.0',
+    'ghp-import>=0.4.1',
+]
 
 TESTS_REQUIRE = [
-                'coverage>=4.0.3',
-                'coveralls>=1.1',
-                'pytest-cov>=2.2.1',
-                'pytest-flakes>=1.0.1',
-                'pytest-pep8>=1.0.6',
-                ]
+    'coverage>=4.0.3',
+    'coveralls>=1.1',
+    'pytest-cov>=2.2.1',
+    'pytest-flakes>=1.0.1',
+    'pytest-pep8>=1.0.6',
+]
 
 
 if __name__ == "__main__":
     from setuptools import setup
     setup(
-          name=DISTNAME,
-          version=VERSION,
-          license=LICENSE,
-          description=DESCRIPTION,
-          long_description=LONG_DESCRIPTION,
-          maintainer=MAINTAINER,
-          url=URL,
-          download_url=DOWNLOAD_URL,
+        name=DISTNAME,
+        version=VERSION,
+        license=LICENSE,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        maintainer=MAINTAINER,
+        url=URL,
+        download_url=DOWNLOAD_URL,
 
-          classifiers=[
-                      'Development Status :: 3 - Alpha',
-                      'Environment :: Console',
-                      'Intended Audience :: Developers',
-                      'Intended Audience :: Science/Research',
-                      'License :: OSI Approved :: BSD License',
-                      'Programming Language :: Python',
-                      'Programming Language :: Python :: 2.7',
-                      'Programming Language :: Python :: 3.4',
-                      'Programming Language :: Python :: 3.5',
-                      'Topic :: Scientific/Engineering',
-                      'Operating System :: Microsoft :: Windows',
-                      'Operating System :: POSIX',
-                      'Operating System :: Unix',
-                      'Operating System :: MacOS',
-                      ],
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: BSD License',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Topic :: Scientific/Engineering',
+            'Operating System :: Microsoft :: Windows',
+            'Operating System :: POSIX',
+            'Operating System :: Unix',
+            'Operating System :: MacOS',
+        ],
 
-          install_requires=INSTALL_REQUIRES,
-          tests_require=TESTS_REQUIRE,
+        install_requires=INSTALL_REQUIRES,
+        tests_require=TESTS_REQUIRE,
 
-          packages=["mousestyles", "mousestyles.tests", "mousestyles.data", 
-              "mousestyles.data.tests", "mousestyles.classification", 
-              "mousestyles.classification.tests", "mousestyles.path_diversity",
-	      "mousestyles.path_diversity.tests", "mousestyles.visualization"],
-          package_data={'mousestyles.data': ['*.npy', '*/*/*.npy']}
-          )
+
+        packages=["mousestyles", "mousestyles.tests",
+                  "mousestyles.data", "mousestyles.data.tests",
+                  "mousestyles.classification", "mousestyles.classification.tests",
+                  "mousestyles.path_diversity", "mousestyles.path_diversity.tests"],
+        package_data={'mousestyles.data': ['*.npy', '*/*/*.npy']}
+    )
