6db97edc38d1cb9599d6e0183523ea10f8a3c5ae
Xinyue Zhou <xinyue233@berkeley.edu>
2016-05-02 10:31:22 -0700

    fix space again and fix module import name

--- mousestyles/est_power_param.py
+++ mousestyles/est_power_param.py
@@ -7,6 +7,7 @@ import statistics as stat
 import pandas as pd
 from mousestyles import data
 
+
 def getdistance(strain, mouse, day):
     """
     Return the distance of each two consecutive points among coordinates which is bigger than 1cm(truncated).
@@ -31,15 +32,16 @@ def getdistance(strain, mouse, day):
     array([ 1.00648944,  1.02094319,  1.0178885 , ...,  1.00099351,
     1.01191156,  1.00423354])
     """
-    df  = data.load_movement(strain, mouse, day)
+    df = data.load_movement(strain, mouse, day)
     xcood = df["x"]
     ycood = df["y"]
-    distance_vector = np.sqrt(np.diff(xcood)**2+np.diff(ycood)**2)
+    distance_vector = np.sqrt(np.diff(xcood)**2 + np.diff(ycood)**2)
     msk = distance_vector > 1
     cut_dist = distance_vector[msk]
     return(cut_dist)
 
-def fit_powerLaw (strain, mouse, day):
+
+def fit_powerLaw(strain, mouse, day):
     """
     Return the estimator of truncated power law.
 
@@ -51,7 +53,7 @@ def fit_powerLaw (strain, mouse, day):
         the mouse number in its strain
     day    :  int
         the day number
- 
+
     Returns
     -------
     estimator : a float number
@@ -62,11 +64,13 @@ def fit_powerLaw (strain, mouse, day):
     >>> fit_powerLaw (0, 0, 0)
     9.4748705008269827
     """
-    cut_dist=getdistance(strain, mouse, day)
-    ret_mle = 1+len(cut_dist)*1/(np.sum(np.log(cut_dist/np.min(cut_dist))))
+    cut_dist = getdistance(strain, mouse, day)
+    ret_mle = 1 + len(cut_dist) * 1 / \
+        (np.sum(np.log(cut_dist / np.min(cut_dist))))
     return ret_mle
 
-def fit_exponential (strain, mouse, day):
+
+def fit_exponential(strain, mouse, day):
     """
     Return the estimator of truncated exponential.
 
@@ -89,8 +93,8 @@ def fit_exponential (strain, mouse, day):
     >>> fit_exponential (0, 0, 0)
     7.385844980814098
     """
-    cut_dist=getdistance(strain, mouse, day)
-    ret_mle = len(cut_dist)/(np.sum(cut_dist)-len(cut_dist))
+    cut_dist = getdistance(strain, mouse, day)
+    ret_mle = len(cut_dist) / (np.sum(cut_dist) - len(cut_dist))
     return ret_mle
 
 
@@ -111,19 +115,20 @@ def fit():
     >>> fit()
     7.385844980814098
     """
-    esti_df = {"strain":[], "mouse":[],"day":[], "power":[], "exp":[]}
+    esti_df = {"strain": [], "mouse": [], "day": [], "power": [], "exp": []}
     for i in range(3):
         for j in range(4):
             for k in range(12):
                 try:
-                    temp1 = fit_powerLaw(i,j,k)
-                    temp2 = fit_exponential(i,j,k)
+                    temp1 = fit_powerLaw(i, j, k)
+                    temp2 = fit_exponential(i, j, k)
                     esti_df["strain"].append(i)
                     esti_df["mouse"].append(j)
                     esti_df["day"] .append(k)
                     esti_df["power"] .append(temp1)
-                    esti_df["exp"] .append(temp2)                
+                    esti_df["exp"] .append(temp2)
                 except:
                     next
-    estimation = pd.DataFrame(esti_df, columns = ["strain", "mouse", "day", "power", "exp"])
+    estimation = pd.DataFrame(
+        esti_df, columns=["strain", "mouse", "day", "power", "exp"])
     return estimation
