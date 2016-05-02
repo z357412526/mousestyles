9aba59dbcd7c3875e1476f63b710b14cde32e0b1
Xinyue Zhou <xinyue233@berkeley.edu>
2016-05-02 11:27:35 -0700

    pep8 fix

--- mousestyles/tests/test_est_power_param.py
+++ mousestyles/tests/test_est_power_param.py
@@ -1,8 +1,7 @@
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 
-import numpy as np
-import pandas as pd
+import pandas
 from mousestyles.est_power_param import (fit_powerLaw, fit_exponential, fit)
 
 
