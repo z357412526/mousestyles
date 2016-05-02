035eb9a8fff712dc92af97ba73b3f1401c13c5b2
Xinyue Zhou <xinyue233@berkeley.edu>
2016-05-02 10:26:30 -0700

    fit space

--- mousestyles/tests/test_est_power_param.py
+++ mousestyles/tests/test_est_power_param.py
@@ -1,4 +1,4 @@
-from __future__ import (absolute_import, division, print_function, unicode_literals)
+from __future__ import (absolute_import, division, print_function, unicode_literals)
 
 import numpy as np
 from mousestyles.powerlaw import (fit_powerLaw,fit_exponential,fit)
