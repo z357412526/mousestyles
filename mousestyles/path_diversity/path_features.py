740bf3375dcbd8c1a19dc02c29a009031833ba60
Jamie Palumbo <jcpalumbo@berkeley.edu>
2016-05-06 18:26:14 -0700

    STY: fixed PEP8 failures

--- mousestyles/path_diversity/path_features.py
+++ mousestyles/path_diversity/path_features.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import math
 
+
 def compute_accelerations(speeds, timestamps):
     r"""
     Returns a list of the accelerations along the path.
@@ -12,7 +13,8 @@ def compute_accelerations(speeds, timestamps):
     Parameters
     ----------
     speeds : list
-        the traveled distances along the path. Expecting the output of compute_distances.
+        the traveled distances along the path. Expecting the output of
+        compute_distances.
 
     timestamps : list
         the time difference within the path.
@@ -27,23 +29,26 @@ def compute_accelerations(speeds, timestamps):
 
     Examples
     --------
-    >>> compute_accelerations([1, 2, 0], [3,4,5,6])
+    >>> compute_accelerations([1, 2, 0], [3, 4, 5, 6])
     [0.5, -1.0]
     """
 
-    if type(speeds) is not list or type(timestamps) is not list:
+    if not isinstance(speeds, list) or not isinstance(timestamps, list):
         raise TypeError("speeds and timestamps must be lists")
 
-    if len(speeds) is not len(timestamps)-1:
-        raise ValueError("lengths of speeds must be the length of timestamps minus 1")
-    
-    speeds_diff = [ x - y for x,y in zip(speeds[:len(speeds)], speeds[1:])] 
-    time_diffs = [ x - y for x,y in zip(timestamps[:len(timestamps)-1], timestamps[2:])]
+    if len(speeds) is not len(timestamps) - 1:
+        raise ValueError(
+            "lengths of speeds must be the length of timestamps minus 1")
 
+    speeds_diff = [x - y for x, y in zip(speeds[:len(speeds)], speeds[1:])]
+    time_diffs = [x - y for x, y in zip(timestamps[:len(timestamps) - 1],
+                  timestamps[2:])]
+
+    test = "timestamps should not contain same times in i th and i+2 th rows."
     if np.count_nonzero(time_diffs) is not len(time_diffs):
-        raise ValueError("timestamps should not contain same times in i th and i+2 th rows.")
+        raise ValueError(test)
 
-    accel = [v/t for v,t in zip(speeds_diff, time_diffs)]
+    accel = [v / t for v, t in zip(speeds_diff, time_diffs)]
     return(accel)
 
 
@@ -51,7 +56,7 @@ def angle_between(v1, v2):
     r"""
     Returns the angle in radians between vectors `v1` and `v2`.
     Both vectors must have same length.
-    
+
     Parameters
     ----------
     v1, v2 : lists
@@ -62,7 +67,7 @@ def angle_between(v1, v2):
     Returns
     -------
     angle : numpy float object
-        nan if either of `v1` or `v2` is zero vector, 
+        nan if either of `v1` or `v2` is zero vector,
 
 
     Examples
@@ -75,28 +80,28 @@ def angle_between(v1, v2):
     3.1415926535897931
     """
 
-    if type(v1) is not list or type(v2) is not list:
+    if not isinstance(v1, list) or not isinstance(v2, list):
         raise TypeError("v1 and v2 must be lists")
 
     if len(v1) is not len(v2):
-        raise ValueError("both vectors must have same lengths")   
+        raise ValueError("both vectors must have same lengths")
 
     norm_v1 = np.linalg.norm(v1)
     norm_v2 = np.linalg.norm(v2)
-    
+
     if np.count_nonzero([norm_v1, norm_v2]) is not 2:
         raise ValueError('both vectors must have norms greater than 0')
-    
+
     v1_u = v1 / norm_v1
     v2_u = v2 / norm_v2
     return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
-    
+
 
 def compute_angles(path_obj, radian=None):
     r"""
     Returns a list of the angles in the path.
-    Each element of the list is the angle between the adjacent vectors in the path.
-    The length of the list is equal to the length of speeds minus 2.
+    Each element of the list is the angle between the adjacent vectors in the
+    path. The length of the list is equal to the length of speeds minus 2.
 
     Parameters
     ----------
@@ -105,7 +110,8 @@ def compute_angles(path_obj, radian=None):
         Must have length greater than 3.
 
     radian : boolean
-        True for the output in radians. False for in turns (i.e. 360 for a full turn).
+        True for the output in radians. False for in turns (i.e. 360 for a
+        full turn).
         Default is False.
 
     Returns
@@ -115,29 +121,31 @@ def compute_angles(path_obj, radian=None):
 
     Examples
     --------
-    >>> path = pd.DataFrame({'t':[2,4.5,10.5], 'x':[0,1,1],\ 
-    'y':[0,0,1], 'isHB':[True,True,False]})
+    >>> path = pd.DataFrame({'t':[2, 4.5, 10.5], 'x':[0, 1, 1],\
+    'y':[0, 0, 1], 'isHB':[True, True, False]})
     >>> compute_angles(path)
     [None, 90.0, None]
     """
-    if radian == None:
+    if radian is None:
         radian = False
-    
-    if type(path_obj) is not pd.core.frame.DataFrame:
+
+    if not isinstance(path_obj, pd.core.frame.DataFrame):
         raise TypeError("path_obj must be pandas DataFrame")
-    
-    if set(path_obj.keys()).issuperset(['x','y']) == False:
+
+    if not set(path_obj.keys()).issuperset(['x', 'y']):
         raise ValueError("the keys of path_obj must contain 'x', 'y'")
-    
+
     if len(path_obj) <= 2:
         raise ValueError("path_obj must contain at least 3 rows")
 
-    if type(radian) != bool:
+    if not isinstance(radian, bool):
         raise TypeError("radian must be bool")
 
-    indices = path_obj.index[:len(path_obj)-1]
-    vectors = [path_obj.loc[i + 1,'x':'y'] - path_obj.loc[i,'x':'y'] for i in indices]
-    angles = [angle_between(list(v1),list(v2)) for v1,v2 in zip(vectors[1:], vectors[:len(vectors)])]
+    indices = path_obj.index[:len(path_obj) - 1]
+    vectors = [path_obj.loc[i + 1, 'x':'y'] - path_obj.loc[i, 'x':'y']
+               for i in indices]
+    angles = [angle_between(list(v1), list(v2)) for v1, v2 in
+              zip(vectors[1:], vectors[:len(vectors)])]
 
     if not radian:
         angles = [theta * 180 / math.pi for theta in angles]
--- mousestyles/path_diversity/tests/test_path_features.py
+++ mousestyles/path_diversity/tests/test_path_features.py
@@ -1,92 +1,98 @@
-from __future__ import (absolute_import, division, print_function, unicode_literals)
+from __future__ import (
+    absolute_import,
+    division,
+    print_function,
+    unicode_literals)
 
 
 import pytest
+import numpy as np
+import pandas as pd
 
-
-from mousestyles import data
-from mousestyles.path_features import compute_accelerations, angle_between, compute_angles
+from mousestyles.path_diversity import path_features
 
 
 def test_compute_accelerations_input():
     # Check if function raises the correct type of errors.
- 
     with pytest.raises(TypeError) as excinfo:
-        compute_accelerations(0, 1)
+        path_features.compute_accelerations(0, 1)
     assert excinfo.value.args[0] == 'speeds and timestamps must be lists'
-    
+
     with pytest.raises(TypeError) as excinfo:
-        compute_accelerations([0], 1)
+        path_features.compute_accelerations([0], 1)
     assert excinfo.value.args[0] == 'speeds and timestamps must be lists'
 
     with pytest.raises(ValueError) as excinfo:
-        compute_accelerations([0,1], [1,2])
+        path_features.compute_accelerations([0, 1], [1, 2])
     assert excinfo.value.args[0] == \
-    'lengths of speeds must be the length of timestamps minus 1'
+        'lengths of speeds must be the length of timestamps minus 1'
 
+    test = 'timestamps should not contain same times in i th and i+2 th rows.'
     with pytest.raises(ValueError) as excinfo:
-        compute_accelerations([1,3], [4,4,4])
-    assert excinfo.value.args[0] == \
-     'timestamps should not contain same times in i th and i+2 th rows.'
+        path_features.compute_accelerations([1, 3], [4, 4, 4])
+    assert excinfo.value.args[0] == test
 
 
 def test_compute_accelerations():
-    assert compute_accelerations([1, 2, 0], [3,4,5,6]) == [0.5, -1.0]
+    assert path_features.compute_accelerations([1, 2, 0],
+                                               [3, 4, 5, 6]) == [0.5, -1.0]
 
 
 def test_angle_between_input():
     # Check if function raises the correct type of errors.
-
     with pytest.raises(TypeError) as excinfo:
-        angle_between([0], 1)
+        path_features.angle_between([0], 1)
     assert excinfo.value.args[0] == 'v1 and v2 must be lists'
-    
+
     with pytest.raises(ValueError) as excinfo:
-        angle_between([0], [1,2])
+        path_features.angle_between([0], [1, 2])
     assert excinfo.value.args[0] == 'both vectors must have same lengths'
 
     with pytest.raises(ValueError) as excinfo:
-        angle_between([0,0], [1,0])
-    assert excinfo.value.args[0] == 'both vectors must have norms greater than 0'
+        path_features.angle_between([0, 0], [1, 0])
+    assert excinfo.value.args[
+        0] == 'both vectors must have norms greater than 0'
 
 
 def test_angle_between():
-    assert angle_between([0,1], [0,1]) == 0.0
-    assert angle_between([1,0], [0,1]) == np.pi/2
-    assert angle_between([0,1], [0,-1]) == np.pi
+    assert path_features.angle_between([0, 1], [0, 1]) == 0.0
+    assert path_features.angle_between([1, 0], [0, 1]) == np.pi / 2
+    assert path_features.angle_between([0, 1], [0, -1]) == np.pi
 
 
 # similar test function for input as compute_distances and take_time_diffs
 def test_compute_angles_input():
     # Check if function raises the correct type of errors.
-    
-    path = pd.DataFrame({'t':[2, 1, -1], 'x':[5, 3, 8], \
-                         'y':[-3, 0, 0], 'isHB':[True, False, False]})
+    path = pd.DataFrame({'t': [2, 1, -1], 'x': [5, 3, 8],
+                         'y': [-3, 0, 0], 'isHB': [True, False, False]})
     # not pandas DataFrame
-    like_path = [1,2,3]
+    like_path = [1, 2, 3]
     # not having keys 'x', 'y'
-    like_path2 = pd.DataFrame({'x':[5,-2,1],'t':[-2,3,1],'isHB':[True,True,False]})
+    like_path2 = pd.DataFrame(
+        {'x': [5, -2, 1], 't': [-2, 3, 1], 'isHB': [True, True, False]})
     # length is less than 2
-    like_path3 = pd.DataFrame({'t':[2], 'x':[5],'y':[3],'isHB':[True]})
-    
+    like_path3 = pd.DataFrame({'t': [2], 'x': [5], 'y': [3], 'isHB': [True]})
+
     with pytest.raises(TypeError) as excinfo:
-        compute_angles(path, 1)
+        path_features.compute_angles(path, 1)
     assert excinfo.value.args[0] == 'radian must be bool'
-    
+
     with pytest.raises(TypeError) as excinfo:
-        compute_angles(like_path, True)
+        path_features.compute_angles(like_path, True)
     assert excinfo.value.args[0] == "path_obj must be pandas DataFrame"
 
     with pytest.raises(ValueError) as excinfo:
-        compute_angles(like_path2, True)
-    assert excinfo.value.args[0] == "the keys of path_obj must contain 'x', 'y'"
+        path_features.compute_angles(like_path2, True)
+    assert excinfo.value.args[
+        0] == "the keys of path_obj must contain 'x', 'y'"
 
     with pytest.raises(ValueError) as excinfo:
-        compute_angles(like_path3, True)
+        path_features.compute_angles(like_path3, True)
     assert excinfo.value.args[0] == "path_obj must contain at least 3 rows"
 
 
 def test_compute_angles():
-    path = pd.DataFrame({'t':[2,4.5,10.5], 'x':[0,1,1],'y':[0,0,1],'isHB':[True,True,False]})
-    assert compute_angles(path) == [None, 90.0, None]
-    assert compute_angles(path, True) == [None, np.pi/2, None]
+    path = pd.DataFrame({'t': [2, 4.5, 10.5], 'x': [0, 1, 1], 'y': [
+                        0, 0, 1], 'isHB': [True, True, False]})
+    assert path_features.compute_angles(path) == [None, 90.0, None]
+    assert path_features.compute_angles(path, True) == [None, np.pi / 2, None]
