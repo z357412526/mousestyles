9dd0f2265da3dc32fac79792ebb1e0f75301ce0e
Jarrod Millman <jarrod.millman@gmail.com>
2016-05-09 14:52:51 -0700

    BUG: fix typos

--- doc/source/report/plots/plot_distribution.py
+++ doc/source/report/plots/plot_distribution.py
@@ -1,14 +1,15 @@
-import matplotlib.pyplot as plt
-from mousestyles import data
-from mousestypes.est_power_param import fit
+from mousestyles.est_power_param import fit
+from mousestyles.visualization.distribution_plot import (plot_powerlaw,
+                                             plot_exponential,
+                                             plot_fitted)
 
-Estimation=fit()
+estimation = fit()
 
-# Plot the histogram of the parameters of powerlaw 
-plot_powerlaw(Estimation)
+# Plot the histogram of the parameters of powerlaw
+plot_powerlaw(estimation)
 
 # Plot the histogram of the parameters of exponential
-plot_expoential(Estimation)
+plot_exponential(estimation)
 
 # Plot the histogram and fitted curve for strain 0, mouse 2, day 5
 plot_fitted(0, 2, 5)
