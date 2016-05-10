55ea761c8b69a4a3f0256b1f7a1002c7795d4c50
Tomofumi Ogawa <togawa28@berkeley.edu>
2016-05-09 22:42:48 -0700

    ENH, BUG: added linewidth option and modified var name.

--- mousestyles/visualization/plot_path.py
+++ mousestyles/visualization/plot_path.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-def plot_path(movement, sep_points, title='example plot of path', alpha=.1,
-              xlim=[-16.24, 3.76], ylim=[0.9, 43.5]):
+def plot_path(movement, paths, title='example plot of path', alpha=.1,
+              linewidth=1., xlim=[-16.24, 3.76], ylim=[0.9, 43.5]):
     r"""
     Plot the lines along paths.
 
@@ -12,17 +12,19 @@ def plot_path(movement, sep_points, title='example plot of path', alpha=.1,
     movement : pandas.DataFrame
         CX, CY coordinates. Must have length greater than 1.
 
-    sep_points : list
-        list of path indicies.
-        Expecting the output of path_index.
+    paths: list
+		a list containing the indices for all paths.
 
     title : str
         the title of the plot. Default is 'example plot of path'
 
     alpha : numeric
         graphical parameter which determines strongness of each
-        line. Default is 1.
+        line. Default is .1.
 
+    linewidth : numeric
+    	graphical parameter which determines the width of each
+    	line. Default is 1.
     xlim, ylim : list
         list of length 2 indicating the end points of the plot.
 
@@ -46,11 +48,11 @@ def plot_path(movement, sep_points, title='example plot of path', alpha=.1,
     if len(movement) <= 1:
         raise ValueError("movement must contain at least 2 rows")
 
-    if not isinstance(sep_points, list):
-        raise TypeError("sep_points must be a list")
+    if not isinstance(paths, list):
+        raise TypeError("paths must be a list")
 
-    if len(sep_points) == 0:
-        raise ValueError("length of sep_points is 0")
+    if len(paths) == 0:
+        raise ValueError("length of paths is 0")
 
     if not isinstance(title, str):
         raise TypeError("title must be a string")
@@ -58,9 +60,13 @@ def plot_path(movement, sep_points, title='example plot of path', alpha=.1,
     if not isinstance(alpha, float):
         raise TypeError("alpha must be float")
 
-    for sep in sep_points:
+    if not isinstance(linewidth, float):
+        raise TypeError("linewidth must be float")
+
+    for sep in paths:
         path = movement[sep[0]:sep[1] + 1]
-        plt.plot(path['x'], path['y'], 'b', alpha=alpha)
+        plt.plot(path['x'], path['y'], 'b',
+        linewidth=linewidth, alpha=alpha)
         plt.xlabel('x-coordinate')
         plt.xlim(xlim[0], xlim[1])
         plt.ylabel('y-coordinate')
